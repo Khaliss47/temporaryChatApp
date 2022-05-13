44392e126494c8e3d3743828a345187923b1f3a1
admiralsimo <thementaliste08@gmail.com>
2022-05-13 21:39:53 +0000

    Chat message prototype data removed

--- pages/components/Chat.jsx
+++ pages/components/Chat.jsx
@@ -19,56 +19,7 @@ import {
   
   
   const messages = [
-    {
-      message: "sdfsdfsdfdsf",
-      from: "others",
-      dateSent: "20:21",
-    },
-    {
-      message: "Sure! At 11:00 am?",
-      from: "me",
-      dateSent: "20:22",
-    },
-    {
-      message: "That's too early! How about at noon?",
-      from: "others",
-      dateSent: "20:22",
-    },
-    {
-      message: "That sounds good as well. Where should we meet?",
-      from: "me",
-      dateSent: "20:23",
-    },
-    {
-      message: "Meet me at the hardware store on 21 Duck Street.",
-      from: "others",
-      dateSent: "20:23",
-    },
-    {
-      message: "Sounds good. I'll bring my friend with me as well!",
-      from: "me",
-      dateSent: "20:24",
-    },
-    {
-      message: "Which one? The developer or the designer?",
-      from: "others",
-      dateSent: "20:24",
-    },
-    {
-      message: "The developer. You remember Tony, right?",
-      from: "me",
-      dateSent: "20:24",
-    },
-    {
-      message: "Yeah! Tony's a great guy!",
-      from: "others",
-      dateSent: "20:25",
-    },
-    {
-      message: "Indeed he is! Alright, see you later 👋!",
-      from: "me",
-      dateSent: "20:25",
-    },
+    
   ];
   
   function Chat() {
